efeb595906c4337fdb3566dce86f0536552ae3c9
NHTrucco <nhtrucco@gmail.com>
2024-07-30 18:06:48 -0300

    Version04

--- js/app.js
+++ js/app.js
@@ -1,63 +1,37 @@
-const vuelos = [
-    { id: 1, destino: 'España', precio: 500 },
-    { id: 2, destino: 'Francia', precio: 450 },
-    { id: 3, destino: 'Alemania', precio: 600 },
-    { id: 4, destino: 'Italia', precio: 550 },
-    { id: 5, destino: 'Reino Unido', precio: 400 },
-    { id: 6, destino: 'Estados Unidos', precio: 700 },
-    { id: 7, destino: 'Canadá', precio: 650 },
-    { id: 8, destino: 'México', precio: 350 },
-    { id: 9, destino: 'Brasil', precio: 500 },
-    { id: 10, destino: 'Argentina', precio: 450 },
-    { id: 11, destino: 'Chile', precio: 300 },
-    { id: 12, destino: 'Perú', precio: 320 },
-    { id: 13, destino: 'Colombia', precio: 330 },
-    { id: 14, destino: 'Japón', precio: 900 },
-    { id: 15, destino: 'China', precio: 850 },
-    { id: 16, destino: 'Australia', precio: 1000 },
-    { id: 17, destino: 'Nueva Zelanda', precio: 950 },
-    { id: 18, destino: 'Sudáfrica', precio: 800 },
-    { id: 19, destino: 'Egipto', precio: 700 },
-    { id: 20, destino: 'India', precio: 750 }
-];
-
 function buscarVuelos() {
-    let busqueda = prompt("Ingresa el criterio de búsqueda (precio/pais):").toLowerCase();
+    let busqueda = document.querySelector(`input`).value.toLowerCase();
+    let precio = !isNaN(busqueda);
+    let resultados;
 
-    if (busqueda === 'precio') {
-        let precioBuscado = parseInt(prompt("Ingresa el precio del vuelo que deseas buscar:"));
-        let resultados = vuelos.filter(vuelo => vuelo.precio <= precioBuscado);
-
-        if (resultados.length > 0) {
-         mostrarEnHtml(resultados);
-        } else {
-            alert("No se encontraron vuelos por ese precio.");
-        }
-
-    } else if (busqueda === 'pais') {
-        let destinoBuscado = prompt("Ingresa el país de destino del vuelo que deseas buscar:").toLowerCase();
-        let resultados = vuelos.filter(vuelo => vuelo.destino.toLowerCase() === destinoBuscado);
-
-        if (resultados.length > 0) {
-         mostrarEnHtml(resultados);
-        } else {
-            alert("No se encontraron vuelos a ese destino.");
-        }
-
+    if (precio) {
+        let precioBuscado = parseFloat(busqueda);
+        resultados = vuelos.filter(vuelo => vuelo.precio <= precioBuscado);
     } else {
-        alert("Criterio de búsqueda no válido. Por favor, ingresa 'precio' o 'pais'.");
+        resultados = vuelos.filter(vuelo => vuelo.destino.toLowerCase().includes(busqueda));
+    }
+
+    if (resultados.length > 0) {
+        mostrarEnHtml(resultados);
+    } else {
+        alert(`No se encontraron vuelos con esos parametros.`);
     }
 }
 
 function mostrarEnHtml(resultados) {
-    let lista = document.getElementById('resultadosVuelos');
-    lista.innerHTML = ''; 
+    let lista = document.getElementById("resultadosVuelos");
+    lista.innerHTML = "";
 
-    const totalImpuesto = 0.30 * 0.45 * 0.25 * 5;
+    const impuesto = 0.3 + 0.45 + 0.25;
+    const template = document.getElementById("vueloTemplate");
 
     resultados.forEach(vuelo => {
-        let li = document.createElement('li');
-        li.textContent = `ID: ${vuelo.id}, Destino: ${vuelo.destino}, Precio: ${vuelo.precio} USD || Valor total con impuestos: ${vuelo.precio + (vuelo.precio * totalImpuesto)} USD`;
-        lista.appendChild(li);
+        let clone = template.content.cloneNode(true);
+        clone.querySelector(`.destino`).textContent = `${vuelo.destino}`;
+        clone.querySelector(`.precio`).textContent = `Precio: ${vuelo.precio} USD`
+        clone.querySelector(`.precioMasImpuesto`).textContent =`Valor total con impuestos: ${(vuelo.precio * (1 + impuesto)).toFixed(2)} USD`;
+        lista.appendChild(clone);
     });
 }
+
+document.querySelector(`button`).addEventListener(`click`, buscarVuelos);
+
---
+++ js/variableVuelos.js
@@ -0,0 +1,51 @@
+/** @format */
+
+class Vuelo {
+   constructor(destino, precio) {
+      this.destino = destino;
+      this.precio = precio;
+   }
+}
+
+const vuelos = [
+   new Vuelo("España", 500),
+   new Vuelo("Francia", 450),
+   new Vuelo("Alemania", 600),
+   new Vuelo("Italia", 550),
+   new Vuelo("Reino Unido", 400),
+   new Vuelo("Estados Unidos", 700),
+   new Vuelo("Canadá", 650),
+   new Vuelo("México", 350),
+   new Vuelo("Brasil", 500),
+   new Vuelo("Argentina", 450),
+   new Vuelo("Chile", 300),
+   new Vuelo("Perú", 320),
+   new Vuelo("Colombia", 330),
+   new Vuelo("Japón", 900),
+   new Vuelo("China", 850),
+   new Vuelo("Australia", 1000),
+   new Vuelo("Nueva Zelanda", 950),
+   new Vuelo("Sudáfrica", 800),
+   new Vuelo("Egipto", 700),
+   new Vuelo("India", 750),
+   new Vuelo("Rusia", 770),
+   new Vuelo("Corea del Sur", 820),
+   new Vuelo("Filipinas", 620),
+   new Vuelo("Indonesia", 670),
+   new Vuelo("Malasia", 710),
+   new Vuelo("Singapur", 760),
+   new Vuelo("Tailandia", 580),
+   new Vuelo("Vietnam", 540),
+   new Vuelo("Turquía", 680),
+   new Vuelo("Grecia", 530),
+   new Vuelo("Portugal", 480),
+   new Vuelo("Noruega", 750),
+   new Vuelo("Suecia", 730),
+   new Vuelo("Dinamarca", 720),
+   new Vuelo("Finlandia", 740),
+   new Vuelo("Polonia", 500),
+   new Vuelo("Suiza", 620),
+   new Vuelo("Austria", 650),
+   new Vuelo("Bélgica", 590),
+   new Vuelo("Países Bajos", 610),
+];
