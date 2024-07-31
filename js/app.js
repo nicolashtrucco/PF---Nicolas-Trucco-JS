3a2d6bf6f59385d990c91bba8f410083ce1616b8
NHTrucco <nhtrucco@gmail.com>
2024-07-31 13:02:18 -0300

    Version07

--- js/app.js
+++ js/app.js
@@ -1,53 +1,64 @@
+/** @format */
+
 function buscarVuelos() {
-   let busqueda = document.querySelector('input').value.toLowerCase();
+   let busqueda = document.querySelector("input").value.toLowerCase();
    let precio = !isNaN(busqueda);
    let resultados;
 
    if (precio) {
-       let precioBuscado = parseFloat(busqueda);
-       resultados = vuelos.filter(vuelo => vuelo.precio <= precioBuscado);
+      let precioBuscado = parseFloat(busqueda);
+      resultados = vuelos.filter((vuelo) => vuelo.precio <= precioBuscado);
    } else {
-       resultados = vuelos.filter(vuelo => vuelo.destino.toLowerCase().includes(busqueda));
+      resultados = vuelos.filter((vuelo) =>
+         vuelo.destino.toLowerCase().includes(busqueda)
+      );
    }
 
    if (resultados.length > 0) {
-       mostrarEnHtml(resultados);
+      mostrarEnHtml(resultados);
    } else {
-       alert('No se encontraron vuelos con esos parametros.');
+      alert("No se encontraron vuelos con esos parametros.");
    }
 }
 
 function mostrarEnHtml(resultados) {
-   let lista = document.getElementById('resultadosVuelos');
-   lista.innerHTML = '';
+   let lista = document.getElementById("resultadosVuelos");
+   lista.innerHTML = "";
 
    const impuesto = 0.3 + 0.45 + 0.25;
-   const template = document.getElementById('vueloTemplate');
+   const template = document.getElementById("vueloTemplate");
 
-   resultados.forEach(vuelo => {
-       let clone = template.content.cloneNode(true);
-       clone.querySelector('.destino').textContent = `${vuelo.destino}`;
-       clone.querySelector('.precio').textContent = `Precio: ${vuelo.precio} USD`
-       clone.querySelector('.precioMasImpuesto').textContent =`Valor total con impuestos: ${(vuelo.precio * (1 + impuesto)).toFixed(2)} USD`;
-       lista.appendChild(clone);
+   resultados.forEach((vuelo) => {
+      let clone = template.content.cloneNode(true);
+      clone.querySelector(".destino").textContent = `${vuelo.destino}`;
+      clone.querySelector(
+         ".precio"
+      ).textContent = `Precio: ${vuelo.precio} USD`;
+      clone.querySelector(
+         ".precioMasImpuesto"
+      ).textContent = `Valor total con impuestos: ${(
+         vuelo.precio *
+         (1 + impuesto)
+      ).toFixed(2)} USD`;
+      lista.appendChild(clone);
    });
 }
 
-document.querySelector('button').addEventListener('click', buscarVuelos);
+document.querySelector("button").addEventListener("click", buscarVuelos);
 
-
-
-document.addEventListener('DOMContentLoaded', function() {
-   const nombreUsuario = localStorage.getItem('nombreUsuario');
+document.addEventListener("DOMContentLoaded", function () {
+   const nombreUsuario = localStorage.getItem("nombreUsuario");
    if (nombreUsuario) {
-       const bienvenidoTemplate = document.getElementById('bienvenidoTemplate').content.cloneNode(true);
-       bienvenidoTemplate.querySelector('.bienvenidoName').textContent = `Bienvenido ${nombreUsuario}`;
-       document.querySelector('.containerPirncipal').prepend(bienvenidoTemplate);
+      const bienvenidoTemplate = document
+         .getElementById("bienvenidoTemplate")
+         .content.cloneNode(true);
+      bienvenidoTemplate.querySelector(
+         ".bienvenidoName"
+      ).textContent = `Bienvenido ${nombreUsuario}`;
+      document.querySelector(".containerPirncipal").prepend(bienvenidoTemplate);
    }
 });
 
-document.querySelector('button').addEventListener('click', buscarVuelos);
+document.querySelector("button").addEventListener("click", buscarVuelos);
 
 console.log(localStorage);
-
-
--- js/login.js
+++ js/login.js
@@ -1,10 +1,10 @@
 /** @format */
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        const inputName = document.querySelector('input').value;
-        localStorage.setItem('nombreUsuario', inputName);
-        window.location.href = './pages/home.html';
-    });
+document.addEventListener("DOMContentLoaded", function () {
+   document.querySelector("form").addEventListener("submit", function (event) {
+      event.preventDefault();
+      const inputName = document.querySelector("input").value;
+      localStorage.setItem("nombreUsuario", inputName);
+      window.location.href = "./pages/home.html";
+   });
 });
